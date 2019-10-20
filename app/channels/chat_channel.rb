9b33def48d92c95e41bad8be4d90d36b8ba0dcec
Brett <brett.simington@gmail.com>
2019-10-20 18:00:16 +0000

    Logic is now working to show who is online

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -3,7 +3,8 @@ class ChatChannel < ApplicationCable::Channel
   def subscribed
     stream_from "chat"
     #current_user.appear
-    user = User.where(params[:user_id])
+    #user = User.where(params[:user_id]) #I think this is grabbing ALL user id's... not the current user id...
+    user = current_user
     user.update(is_online?: '1')
   end
  
